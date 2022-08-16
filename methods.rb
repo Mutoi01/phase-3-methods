88acf1567453157cb68ec0203dd46aeae5f6c214
Mutoi01 <pngando9@gmail.com>
2022-08-16 14:08:57 +0300

    commit tree 6ee32b9d83bc91859c699f5bce06635ae4a395d9

--- methods.rb
+++ methods.rb
@@ -1,1 +1,33 @@
 # Your code here!
+def greet_programmer 
+  puts "Hello, programmer!"
+end
+# greet_programmer
+
+def greet(name)
+
+  puts "Hello, #{name}!"
+  
+ end
+#  greet("kibe")
+
+def greet_with_default (name = "programmer")
+  if name == ""
+    return "Hello,#{name}!"
+  end
+ puts "Hello, #{name}!"
+ 
+end 
+
+ def add (num1,num2)
+  return num1+num2
+end
+
+# add(1,2)
+
+def halve (number)
+if number.is_a? Integer  
+  return number/2
+end
+return nil
+end
